b7241bd493cb6dbf96cf05e24fea932c5d35fb0f
AmadoMiguel <amadodevia@gmail.com>
2019-09-01 22:02:53 -0500

    started MSA for back-end

---
+++ back-end/gateway-service/src/main/java/com/revature/Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}

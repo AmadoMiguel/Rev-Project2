890b088221e5f9992a0612124fc7c970b006903d
AmadoMiguel <amadodevia@gmail.com>
2019-09-02 08:32:56 -0500

    removed service registry

--- back-end/gateway-service/src/main/java/com/revature/Application.java
+++ back-end/gateway-service/src/main/java/com/revature/Application.java
@@ -2,9 +2,12 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+// This way, gateway doesn't have to rely on Eureka
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws Exception {

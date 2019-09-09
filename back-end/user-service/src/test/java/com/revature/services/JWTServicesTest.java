00f3f6f2e9a9ecb1ab2a232016dd65fb86aaf1f0
AmadoMiguel <amadodevia@gmail.com>
2019-09-09 12:06:00 -0500

    finished JWT test on service layer

--- back-end/user-service/src/test/java/com/revature/services/JWTServicesTest.java
+++ back-end/user-service/src/test/java/com/revature/services/JWTServicesTest.java
@@ -18,11 +18,45 @@ public class JWTServicesTest {
 //		Create fake user for the test
 		User fakeUser = new User(1, "username", null, 
 								 null, "user@mail.com", null);
-//		Create assertion to verify returned string is not equal to any random string
+//		Create assertion to verify returned string is not equal to empty string
 		assertThat(JWTService.createJWT(String.valueOf(fakeUser.getId()), fakeUser.getUsername(), 
-										fakeUser.getEmail(), 0)).isNotEqualTo("blabla");
+										fakeUser.getEmail(), 0)).isNotEqualTo("");
 	}
 	
+	@Test
+	public void checkAuthByIdTest() {
+//		Create new user
+		User fakeUser = new User(1, "username", "First", 
+				 				 "Last", "user@mail.com", "hashedpassword");
+//		Create JWT for test
+		String fakeToken = JWTService.createJWT(String.valueOf(fakeUser.getId()), 
+												fakeUser.getUsername(), fakeUser.getEmail(),0);
+//		Create assertion
+		assertThat(JWTService.checkAuthByID(fakeToken, fakeUser.getId())).isTrue();
+	}
 	
+	@Test
+	public void checkAuthTest() {
+//		Create new user
+		User fakeUser = new User(1, "username", "First", 
+				 				 "Last", "user@mail.com", "hashedpassword");
+//		Create JWT for test
+		String fakeToken = JWTService.createJWT(String.valueOf(fakeUser.getId()), 
+												fakeUser.getUsername(), fakeUser.getEmail(),0);
+//		Create assertion
+		assertThat(JWTService.checkAuth(fakeToken)).isTrue();
+	}
+	
+	@Test
+	public void checkAuthByUsernameTest() {
+//		Create new user
+		User fakeUser = new User(1, "username", "First", 
+				 				 "Last", "user@mail.com", "hashedpassword");
+//		Create JWT for test
+		String fakeToken = JWTService.createJWT(String.valueOf(fakeUser.getId()), 
+												fakeUser.getUsername(), fakeUser.getEmail(),0);
+//		Create assertion
+		assertThat(JWTService.checkAuthByUsername(fakeToken,fakeUser.getUsername())).isTrue();
+	}
 	
 }

5bc7f3fa57fe4bf8f0019486224eeebd4398234d
AmadoMiguel <amadodevia@gmail.com>
2019-09-09 11:04:24 -0500

    added create JWT test on service layer

--- back-end/user-service/src/test/java/com/revature/services/JWTServicesTest.java
+++ back-end/user-service/src/test/java/com/revature/services/JWTServicesTest.java
@@ -1,5 +1,28 @@
 package com.revature.services;
 
-public class JWTServicesTest {
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.revature.models.User;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JWTServicesTest {
+	
+	@Test
+	public void createJWTTest() {
+//		Create fake user for the test
+		User fakeUser = new User(1, "username", null, 
+								 null, "user@mail.com", null);
+//		Create assertion to verify returned string is not equal to any random string
+		assertThat(JWTService.createJWT(String.valueOf(fakeUser.getId()), fakeUser.getUsername(), 
+										fakeUser.getEmail(), 0)).isNotEqualTo("blabla");
+	}
+	
+	
+	
 }
